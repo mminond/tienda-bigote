f487c9cd4a045fc08fdbd8adce46245135fb7487
Matias Minond <matiasminond@gmail.com>
2020-12-21 20:15:14 -0300

    Limite de CounContainer por disabled

--- src/components/Product/CountContainer.jsx
+++ src/components/Product/CountContainer.jsx
@@ -1,12 +1,13 @@
 import './CountContainer.scss';
 
-export default function CountContainer({ count, add, less }) {
-    return (
-      <div className="productCount">
-        <button className="productCountButton" onClick={() => less()}>-</button>
-        <p className="productQty">{count}</p>
-        <button className="productCountButton" onClick={() => add()}>+</button>
-      </div>
-    );
-  }
-  
+export default function CountContainer({ stock, count, add, less }) {
+	return (
+		<div className="productCount">
+			<button disabled={count <= 1} className="productCountBtn" onClick={() => less()}>-</button>
+			<p className="productQty">{count}</p>
+			<button disabled={count >= stock} className="productCountBtn" onClick={() => add()}>+</button>
+		</div>
+	);
+}
+
+
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,23 +4,14 @@ import CountContainer from "./CountContainer";
 
 function Product({ id, image, title, price, stock }) {
 	const [count, setCount] = useState(1);
-	const qty = stock;
 
 	const add = () => {
-		if (count < qty) {
+		if (count < stock) {
 			setCount(count + 1);
-		}
-		if (count === qty) {
-			alert("Solo tengo eso en stock");
 		}
 	};
 
 	const less = () => {
-		if (count === 1) {
-			alert("No se puede menos de 1");
-			return;
-		}
-
 		setCount(count - 1);
 	};
 
@@ -30,7 +21,7 @@ function Product({ id, image, title, price, stock }) {
 			<img className="productImg" src={image} alt={"Foto de " + title} />
 			<h3 className="productTitle">{title}</h3>
 			<p className="productPrice">${price}</p>
-			<CountContainer count={count} add={add} less={less} />
+			<CountContainer stock={stock} count={count} add={add} less={less} />
 			<button className="productAdd">Agregar</button>
 		</div>
 	);
