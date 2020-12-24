950cce6e31b1011ec94f5321b381c421040dc688
Matias Minond <matiasminond@gmail.com>
2020-12-24 10:44:53 -0300

    Clases condicionales para boton contador

--- src/components/Product/CountContainer.jsx
+++ src/components/Product/CountContainer.jsx
@@ -3,9 +3,9 @@ import './CountContainer.scss';
 export default function CountContainer({ stock, count, add, less }) {
 	return (
 		<div className="productCount">
-			<button disabled={count <= 1} className="productCountBtn" onClick={() => less()}>-</button>
+			<button disabled={count <= 1} className={`productCountBtn ${count <= 1 ? 'productCountBtn' : 'productCountBtn btnCountActive'}`} onClick={() => less()}>-</button>
 			<p className="productQty">{count}</p>
-			<button disabled={count >= stock} className="productCountBtn" onClick={() => add()}>+</button>
+			<button disabled={count >= stock} className={`productCountBtn ${count >= stock ? 'productCountBtn' : 'productCountBtn btnCountActive'}`} onClick={() => add()}>+</button>
 		</div>
 	);
 }
