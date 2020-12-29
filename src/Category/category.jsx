497f4c8975c8ebdf13bf432d4ea1f36b9f64d4a4
Matias Minond <matiasminond@gmail.com>
2020-12-29 20:07:46 -0300

    Creacion seccion Category

---
+++ src/Category/category.jsx
@@ -0,0 +1,10 @@
+import './category.scss';
+import Itemlistcontainer from "../containers/itemlistcontainer/itemlistcontainer";
+
+function Category() {
+	return (
+        <Itemlistcontainer/>
+	);
+}
+
+export default Category;
