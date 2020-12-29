a7c50f7139c405911887e8aa710582d4d5dd8f46
Matias Minond <matiasminond@gmail.com>
2020-12-29 20:08:12 -0300

    Arreglos en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/global/navbar/navbar';
 import Categories from './components/global/categories/categories';
-import ItemDetailContainer from './containers/itemDetailContainer/itemDetailContainer';
 import Error404 from './Error404/error404';
 import Home from './Home/home';
 import Cart from './Cart/cart';
+import Category from './Category/category'
+import ProductDetail from './ProductDetail/productDetail'
 
 function App() {
 	return (
@@ -19,7 +20,10 @@ function App() {
 					<Cart />
 				</Route>
 				<Route path="/detail">
-					<ItemDetailContainer />
+					<ProductDetail />
+				</Route>
+				<Route path="/category">
+					<Category />
 				</Route>
 				<Route path="*">
 					<Error404 />
