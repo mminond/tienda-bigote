b34cd195ec6fa347ad7c2089b0a7cf907a832214
Matias Minond <matiasminond@gmail.com>
2020-12-29 19:43:04 -0300

    Correccion del itemDetailContainer

--- src/components/productDetail/productDetail.jsx
+++ src/components/productDetail/productDetail.jsx
@@ -1,35 +1,12 @@
-import { useState, useEffect } from 'react';
 import './productDetail.scss';
 
-function ProductDetail() {
-	const [product, setProduct] = useState([]);
-	const item = [{ productId: 0, productTitle: "Muñeca", productImage: 0, productPrice: 150, productStock: 5 }];
-
-
-	const getProduct = new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve(item);
-		}, 2000);
-	});
-
-	useEffect(() => {
-		getProduct.then(rta => setProduct(rta));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
+function ProductDetail({ id, image, title, price, stock }) {
 	return (
 		<article className="productDetail">
-			{
-				product.length ?
-					<>
-						<h1>{item[0].productTitle}</h1>
-						<h2>${item[0].productPrice}</h2>
-						<img src="#" alt="Foto Producto" />
-						<h3>Stock Disponible: {item[0].productStock}</h3>
-					</> :
-					<p className="cargando">Cargando</p>
-			}
-
+			<h1>{title}</h1>
+			<h2>${price}</h2>
+			<img src="#" alt="Foto Producto" />
+			<h3>Stock Disponible: {stock}</h3>
 		</article>
 	);
 }
--- src/containers/itemDetailContainer/itemDetailContainer.jsx
+++ src/containers/itemDetailContainer/itemDetailContainer.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
 import './itemDetailContainer.scss';
 import ProductDetail from '../../components/productDetail/productDetail';
 
 function ItemDetailContainer() {
+	const [product, setProduct] = useState([]);
+	const item = [{ productId: 0, productTitle: "Muñeca", productImage: 0, productPrice: 150, productStock: 5 }];
+
+
+	const getProduct = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(item);
+		}, 2000);
+	});
+
+	useEffect(() => {
+		getProduct.then(rta => setProduct(rta));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<section className="itemDetailContainer">
-			<ProductDetail />
+			{
+				product.length ?
+					<ProductDetail key={item[0].productId}
+						id={item[0].productId}
+						title={item[0].productTitle}
+						image={item[0].productImage}
+						price={item[0].productPrice}
+						stock={item[0].productStock}
+					/> :
+					<p className="cargando">Cargando</p>
+			}
 		</section>
 	);
 }
