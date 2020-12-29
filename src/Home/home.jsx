70748f9d2be21eed759e291ec367073d4fd3bdde
Matias Minond <matiasminond@gmail.com>
2020-12-29 19:15:42 -0300

    Edicion de Home con ItemListContainer

---
+++ src/Home/home.jsx
@@ -0,0 +1,10 @@
+import './home.scss';
+import Itemlistcontainer from "../containers/itemlistcontainer/itemlistcontainer";
+
+function Home() {
+	return (
+        <Itemlistcontainer/>
+	);
+}
+
+export default Home;
