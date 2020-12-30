c935664132deb7e32edad0d125ca8f23a4951b92
Matias Minond <matiasminond@gmail.com>
2020-12-29 21:26:28 -0300

    useParams en Category

--- src/components/global/categories/category.jsx
+++ src/components/global/categories/category.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import './category.scss';
 
 const Category = ({title, id, image})  => (
-	<Link to="/category" id={id} className="btnCategory">
+	<Link to={'/category/' + id} id={id} className="btnCategory">
 		<img className="imgCategory" src={image} alt={"Icono " + title}/>
 		<h5 className="titleCategory">{title}</h5>
 	</Link>
