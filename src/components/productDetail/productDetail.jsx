7f9dfe3f5af350f41550a6f62a215c164f7e2861
Matias Minond <matiasminond@gmail.com>
2021-01-03 21:15:51 -0300

    estilos + Contador en productDetail

--- src/components/productDetail/productDetail.jsx
+++ src/components/productDetail/productDetail.jsx
@@ -1,12 +1,30 @@
 import './productDetail.scss';
+import { useState } from 'react';
+import CountContainer from '../Product/CountContainer';
 
 function ProductDetail({ id, image, title, price, stock }) {
+	const [count, setCount] = useState(1);
+
+	const add = () => {
+		if (count < stock) {
+			setCount(count + 1);
+		}
+	};
+
+	const less = () => {
+		setCount(count - 1);
+	};
 	return (
 		<article className="productDetail">
-			<h1>{title}</h1>
-			<h2>${price}</h2>
-			<img src="#" alt="Foto Producto" />
-			<h3>Stock Disponible: {stock}</h3>
+			<div className="imgProductDetail">
+				<img src="#" alt="Foto Producto" />
+			</div>
+			<div className="descriptionProductDetail">
+				<h1>{title}</h1>
+				<h2>${price}</h2>
+				<h3>Stock Disponible: {stock}</h3>
+				<CountContainer stock={stock} count={count} add={add} less={less} />
+			</div>
 		</article>
 	);
 }
