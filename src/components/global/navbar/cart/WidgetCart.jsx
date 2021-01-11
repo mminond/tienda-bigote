a68f7dab049a94dd4071b61e10e84fe34239e808
Matias Minond <matiasminond@gmail.com>
2021-01-11 17:53:03 -0300

    Creacion clickHandleDelete

--- src/components/global/navbar/cart/WidgetCart.jsx
+++ src/components/global/navbar/cart/WidgetCart.jsx
@@ -6,6 +6,37 @@ import { HiTrash } from "react-icons/hi";
 function WidgetCart({ showWidgetCart }) {
 	// eslint-disable-next-line no-unused-vars
 	const [data, setData] = useContext(Store);
+
+	const handleClickDelete = (e) => {
+		let btnProductId;
+		switch (e.target.localName) {
+			case "path":
+				btnProductId = e.nativeEvent.path[2].id;
+				break;
+			case "svg":
+				btnProductId = e.nativeEvent.path[1].id;
+				break;
+			case "button":
+				btnProductId = e.target.id;
+				break;
+			default:
+				break;
+		}
+		data.items.forEach(item => {
+			console.log(data.items);
+			const indexProduct = data.items.findIndex(item => {
+				return item.item.productId.toString() === btnProductId;
+			});
+			setData({
+				...data,
+				cantidad: data.cantidad - data.items[indexProduct].count,
+				items: [...data.items]
+			});
+			console.log(indexProduct);
+			data.items.splice(indexProduct, 1);
+			console.log(data.items);
+		});
+	}
 	return (
 		<div className={`widgetCart ${showWidgetCart ? 'open' : 'close'}`}>
 			<h3 className="titleWidgetCart">Carrito de Compras</h3>
@@ -14,7 +45,7 @@ function WidgetCart({ showWidgetCart }) {
 					data.items.map(item =>
 						<div className="itemWidgetCart" key={item.item.productId}>
 							<h4 className="itemTitleWidgetCart">{item.item.productTitle} <span className="itemSpanTitleWidgetCart">x{item.count}</span></h4>
-							<button className="itemButtonWidgetCart"><HiTrash size={24} color='white' /></button>
+							<button id={item.item.productId} onClick={handleClickDelete} className="itemButtonWidgetCart"><HiTrash size={24} color='white' /></button>
 						</div>
 					)
 				}
