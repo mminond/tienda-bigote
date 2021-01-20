07fe085770a506e3863a4d5fd7091a6cd6167f4b
Matias Minond <matiasminond@gmail.com>
2021-01-20 16:43:53 -0300

    detalles scss + mensaje final

--- src/components/Checkout/checkout.jsx
+++ src/components/Checkout/checkout.jsx
@@ -13,7 +13,9 @@ function Checkout() {
         email: '',
         tel: '',
     });
-    const handleChangeInput = e =>{
+    const [venta, completarVenta] = useState(false);
+    const [idVenta, setIdVenta] = useState('');
+    const handleChangeInput = e => {
         setFormData({
             ...formData, [e.target.name]: e.target.value
         });
@@ -25,33 +27,38 @@ function Checkout() {
         totalPrice: cart.precioFinal,
         date: firebase.firestore.Timestamp.fromDate(new Date()),
     }
-    const handleSubmitForm = () =>{
+    const handleSubmitForm = () => {
         db.collection('ventas').add(compra)
-        .then(({id}) => {
-            console.log(id);
-        })
-        .catch(e => console.log(e));
+            .then(({ id }) => {
+                setIdVenta(id);
+                completarVenta(true);
+            })
+            .catch(e => console.log(e));
     }
-
-
 
     return (
         <section className="sectionCart">
-            <h3 className="titleCart">Carrito de Compras</h3>
+            <h3 className="titleCart">Checkout</h3>
             <div className="containerCart">
-                <main className="mainForm">
-                    <form>
-                        <input type="text" value={formData.nombre} onChange={handleChangeInput} name="nombre" placeholder="Nombre"/>
-                        <input type="text" value={formData.apellido} onChange={handleChangeInput} name="apellido" placeholder="Apellido"/>
-                        <input type="email" value={formData.email} onChange={handleChangeInput} name="email" placeholder="E-mail"/>
-                        <input type="tel" value={formData.tel} onChange={handleChangeInput} name="tel" placeholder="Teléfono"/>
-                    </form>
-                </main>
-                <aside className="cartLastStep">
-                    <h2 className="finalPrice">${cart.precioFinal}</h2>
-                    <p>Precio Final</p>
-                    <button onClick={handleSubmitForm} className="btnCheckout">Pagar</button>
-                </aside>
+                {
+                    !venta ?
+                        <>
+                            <main className="mainForm">
+                                <form>
+                                    <input type="text" value={formData.nombre} onChange={handleChangeInput} name="nombre" placeholder="Nombre" />
+                                    <input type="text" value={formData.apellido} onChange={handleChangeInput} name="apellido" placeholder="Apellido" />
+                                    <input type="email" value={formData.email} onChange={handleChangeInput} name="email" placeholder="E-mail" />
+                                    <input type="tel" value={formData.tel} onChange={handleChangeInput} name="tel" placeholder="Teléfono" />
+                                </form>
+                            </main>
+                            <aside className="cartLastStep">
+                                <h2 className="finalPrice">${cart.precioFinal}</h2>
+                                <p>Precio Final</p>
+                                <button onClick={handleSubmitForm} className="btnCheckout">Pagar</button>
+                            </aside>
+                        </> :
+                        <p className="mensajeError">La compra se realizó correctamente, tu número de seguimiento es: {idVenta}</p>
+                }
             </div>
         </section>
     );
